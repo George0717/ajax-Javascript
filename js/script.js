7e77cafac2ab6ffd7c76227c656c23bf87c9179d
George0717 <raden7george@gmail.com>
2022-11-15 20:26:00 +0700

    fail

--- js/script.js
+++ js/script.js
@@ -1,19 +1,28 @@
-let jumlahPokemon = document.getElementById('jumlah_pokemon')
-let listPokemon = document.getElementById('list_pokemon')
+let jumlahPokemon = document.getElementById("jumlah_bola");
+let listPokemon = document.getElementById("list_bola");
 
-fetch("https://pokeapi.co/api/v2/pokemon?limit=50")
-.then(response => 
-    response.json())
-.then(data => { 
-    console.log(data)
-    jumlahPokemon.innerHTML = data.count
-    console.log(data.results)   
-    data.results.forEach(showPokemon)
-})
-.catch(error => {
-    console.log(error)  
-});
+fetch("http://127.0.0.1:5500/standings.json")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    jumlahPokemon.innerHTML = data.count;
+    console.log(data.results);
+    data.standings[0].table.forEach(showPokemon);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
-function showPokemon(value, index){
-    listPokemon.innerHTML += `<li>${value.name}</li>`
+function showPokemon(value, index) {
+  listPokemon.innerHTML += `<table class="table">
+  <thead>
+   
+  </thead>
+  <tbody>
+    <tr>
+      
+      <td>${value.name}</td>
+      <td>${value.code}</td>
+      <td>${value.type}</td>
+    </tr>`;
 }
