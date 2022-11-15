fb15bf98afdcf2ba9d01be3d4274f1f4ce406539
George0717 <raden7george@gmail.com>
2022-11-15 20:55:24 +0700

    ..

--- js/script.js
+++ js/script.js
@@ -1,22 +1,19 @@
 let jumlahPokemon = document.getElementById("jumlah_bola");
 let listPokemon = document.getElementById("list_bola");
 
-fetch("http://127.0.0.1:5500/standings.json")
+fetch("http://127.0.0.1:5500/matches.json")
   .then((response) => response.json())
   .then((data) => {
-    console.log(data);
-    jumlahPokemon.innerHTML = data.count;
-    console.log(data.results);
-    data.standings[0].table.forEach(showPokemon);
+    console.log(data.competition);
+    data.competition[0].forEach(showPokemon);
   })
   .catch((error) => {
     console.log(error);
   });
 
 function showPokemon(value, index) {
-  listPokemon.innerHTML += `<table class="table">
-  <thead>
-   
+  listPokemon.innerHTML += `<thead> <tr><th scope="col">#</th><th scope="col">First</th><th scope="col">Last</th><th scope="col">Handle</th>
+    </tr>
   </thead>
   <tbody>
     <tr>
@@ -24,5 +21,6 @@ function showPokemon(value, index) {
       <td>${value.name}</td>
       <td>${value.code}</td>
       <td>${value.type}</td>
-    </tr>`;
+    </tr>
+    </tbody>`;
 }
