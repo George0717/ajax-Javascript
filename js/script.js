916a27e52264a3e01e55ca2e4d551f1ba185414a
George0717 <raden7george@gmail.com>
2022-11-24 08:59:28 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,26 +1,18 @@
-let jumlahPokemon = document.getElementById("jumlah_bola");
-let listPokemon = document.getElementById("list_bola");
+let jumlahPokemon = document.getElementById("jumlah_pokemon");
+let listPokemon = document.getElementById("list_pokemon");
 
-fetch("http://127.0.0.1:5500/matches.json")
+fetch("https://pokeapi.co/api/v2/pokemon?limit=50")
   .then((response) => response.json())
   .then((data) => {
-    console.log(data.competition);
-    data.competition[0].forEach(showPokemon);
+    console.log(data);
+    jumlahPokemon.innerHTML = data.count;
+    console.log(data.results);
+    data.results.forEach(showPokemon);
   })
   .catch((error) => {
     console.log(error);
   });
 
 function showPokemon(value, index) {
-  listPokemon.innerHTML += `<thead> <tr><th scope="col">#</th><th scope="col">First</th><th scope="col">Last</th><th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      
-      <td>${value.name}</td>
-      <td>${value.code}</td>
-      <td>${value.type}</td>
-    </tr>
-    </tbody>`;
+  listPokemon.innerHTML += `<li>${value.name}</li>`;
 }
